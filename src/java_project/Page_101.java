082c5299e437ebbd5a5c9fa0e8f9e1bc6026ee91
unknown <dudals2467@kookmin.ac.kr>
2020-08-28 23:26:40 +0900

    third

---
+++ src/java_project/Page_101.java
@@ -0,0 +1,22 @@
+package java_project;
+
+public class Page_101 {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		int rank = 1;
+		char medalColor;
+		
+		switch(rank) {
+		case 1 : medalColor = 'G';
+		break;
+		case 2 : medalColor = 'S';
+		break;
+		case 3 : medalColor = 'B';
+		break;
+		default : medalColor = 'A';
+		}
+		System.out.println(rank + "등의 메달 색깔은 " + medalColor + " 입니다.");
+	}
+	
+}
---
+++ src/java_project/page_82.java
@@ -0,0 +1,16 @@
+package java_project;
+
+public class page_82 {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		int num = 10;
+		boolean isEven;
+		
+		isEven = ((num % 2) == 0) ? true : false;
+		System.out.println(isEven);
+	}
+
+}
+
+// 조건 연산자 사용하여 10이 짝수인경우 출력
